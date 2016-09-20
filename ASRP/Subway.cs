cb59d957ba87849a026642e5da47989efca7fa12
mycraftmw <mycraftmw.163>
2016-09-21 02:41:00 +0800

    setting the class

---
+++ ASRP/Subway.cs
@@ -0,0 +1,15 @@
+namespace ASRP
+{
+    class Station
+    {
+        public int Id
+        {
+            get { return id; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+}
