1d5fa2d8af9612e654a88c8c898eb83e215508e3
dengchuyun <dengchuyun@hotmial.com>
2016-09-29 16:35:55 +0800

    Revert "adjust."
    This reverts commit 8e674038dfae7d5e827cb1e3e87a4705b128e740.

--- AdvancedSubwayRoutePlanning/Core/BackgroundCore.cs
+++ AdvancedSubwayRoutePlanning/Core/BackgroundCore.cs
@@ -68,9 +68,7 @@ namespace Core
                 }
                 else if (args.Length == 3)
                 {
-                    SubwayMap.SetStartStation(args[1]);
-                    SubwayMap.SetEndStation(args[2]);
-                    route = SubwayMap.GetDirections(args[0]);
+                    route = SubwayMap.GetDirections(args[1], args[2], args[0]);
                     Printer.PrintDirections(route);
                     mainWindow.Close();
                     return;
