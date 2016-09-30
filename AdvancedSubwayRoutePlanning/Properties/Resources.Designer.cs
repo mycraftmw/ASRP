cf9f452cb029ca51e218fadfacebbd581ba2be31
dengchuyun <dengchuyun@hotmial.com>
2016-09-30 22:33:05 +0800

    finish the project.

--- AdvancedSubwayRoutePlanning/Properties/Resources.Designer.cs
+++ AdvancedSubwayRoutePlanning/Properties/Resources.Designer.cs
@@ -89,5 +89,15 @@ namespace AdvancedSubwayRoutePlanning.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
+        internal static System.Drawing.Bitmap TransferStation {
+            get {
+                object obj = ResourceManager.GetObject("TransferStation", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
